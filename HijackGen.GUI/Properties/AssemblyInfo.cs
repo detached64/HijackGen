129460a3101614feeabdfaba0fdc9b0a14e75237
detached64 <169419571+detached64@users.noreply.github.com>
2025-03-26 17:36:14 +0800

    Bump versions

--- HijackGen.GUI/Properties/AssemblyInfo.cs
+++ HijackGen.GUI/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("1.1.0.3")]
-[assembly: AssemblyFileVersion("1.1.0.3")]
+[assembly: AssemblyVersion("1.1.0.4")]
+[assembly: AssemblyFileVersion("1.1.0.4")]
