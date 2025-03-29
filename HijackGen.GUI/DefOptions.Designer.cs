7481ae9a85d4390fe05c5986b0c8fdcd885a098a
detached64 <169419571+detached64@users.noreply.github.com>
2025-03-29 16:23:53 +0800

    Fix

--- HijackGen.GUI/DefOptions.Designer.cs
+++ HijackGen.GUI/DefOptions.Designer.cs
@@ -61,11 +61,13 @@ namespace HijackGen.GUI
             resources.ApplyResources(this.btSelect, "btSelect");
             this.btSelect.Name = "btSelect";
             this.btSelect.UseVisualStyleBackColor = true;
+            this.btSelect.Click += new System.EventHandler(this.btSelect_Click);
             // 
             // txtPath
             // 
             resources.ApplyResources(this.txtPath, "txtPath");
             this.txtPath.Name = "txtPath";
+            this.txtPath.TextChanged += new System.EventHandler(this.txtPath_TextChanged);
             // 
             // lbPath
             // 
@@ -83,6 +85,7 @@ namespace HijackGen.GUI
             resources.ApplyResources(this.btGen, "btGen");
             this.btGen.Name = "btGen";
             this.btGen.UseVisualStyleBackColor = true;
+            this.btGen.Click += new System.EventHandler(this.btGen_Click);
             // 
             // DefOptions
             // 
