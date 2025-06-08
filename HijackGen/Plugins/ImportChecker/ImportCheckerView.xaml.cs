df1ab3cca4d426767885280dfcfedbf3d553114b
detached64 <169419571+detached64@users.noreply.github.com>
2025-06-08 13:20:35 +0800

    Fix DragDrop

--- HijackGen/Plugins/ImportChecker/ImportCheckerView.xaml.cs
+++ HijackGen/Plugins/ImportChecker/ImportCheckerView.xaml.cs
@@ -8,5 +8,21 @@ namespace HijackGen.Plugins.ImportChecker
         {
             InitializeComponent();
         }
+
+        private void Window_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = e.Data.GetDataPresent(DataFormats.FileDrop)
+                ? DragDropEffects.Copy
+                : DragDropEffects.None;
+            e.Handled = true;
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetData(DataFormats.FileDrop) is string[] files && files.Length > 0)
+            {
+                this.TxtPEPath.Text = files[0];
+            }
+        }
     }
 }
